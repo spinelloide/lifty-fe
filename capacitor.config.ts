9f6c34502feb00abe5ea0260edc6c3b4ccbd882a
Paolo Spinelli <paolo.spinelli@beatdata.it>
2025-09-24 16:38:49 +0200

    ADDED CAPACITOR

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lify.app',
+  appName: 'lifty',
+  webDir: 'dist'
+};
+
+export default config;
